f361688c912963a6a60522cf493863a7ecb6bcc4
Marco <dev.marcotullio@gmail.com>
2026-07-05 13:15:00 -0300

    First commit

---
+++ src/components/theme/ThemeProvider.tsx
@@ -0,0 +1,14 @@
+// src/components/theme/ThemeProvider.tsx
+"use client";
+
+import {
+    ThemeProvider as NextThemesProvider,
+    type ThemeProviderProps,
+} from "next-themes";
+
+// Reexporta um provider que aceita TODAS as props do next-themes
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+    return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
+
+
